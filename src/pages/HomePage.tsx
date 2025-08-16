f09efd841699845d13eaddb331b6ed35f4d88dbd
Rosa <rosa.harofdez@gmail.com>
2025-08-16 11:52:59 +0200

    feat(styles): add design tokens and base presets (cards, inputs, buttons)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,10 +5,6 @@ const HomePage = () => {
     <>
       <h1>GitHub Repo Searcher</h1>
       <UserSearchComponent />
-      <div className="bg-blue-500 text-white p-4 rounded">
-  Tailwind is working! 🎉
-</div>
-
     </>
   );
 };
